079c4b921db37fb21a77f3b102747d6df9c709a8
Ruben Helsen <ruben.helsen@keysight.com>
2024-10-14 12:06:20 +0200

    Modularize

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,1 @@
+# Add your custom keymaps here! No changes in the common setup will be placed here, so no merge conflicts!
---
+++ lua/custom/options.lua
@@ -0,0 +1,1 @@
+# Add your custom options here! No changes in the common setup will be placed here, so no merge conflicts!
