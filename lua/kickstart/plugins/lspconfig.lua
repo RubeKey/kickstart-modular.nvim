9cd7589ae9dbb79171eed188da9602bb98a66786
Ruben Helsen <ruben.helsen@keysight.com>
2024-10-17 13:30:09 +0200

    Add vhdl_ls

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -19,7 +19,7 @@ return {
       -- Brief aside: **What is LSP?**
       --
       -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
+
       -- LSP stands for Language Server Protocol. It's a protocol that helps editors
       -- and language tooling communicate in a standardized fashion.
       --
@@ -185,6 +185,7 @@ return {
             },
           },
         },
+        vhdl_ls = {},
         ruff = {
           args = { '--config =' .. root .. '/ruff.toml' },
         },
@@ -214,6 +215,7 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'ruff',
+        'vhdl_ls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
